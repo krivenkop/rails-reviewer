2a83829c3e3afe038c7f4266b01f406370700032
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-06 12:59:46 +0200

    OVERRIDE: devise default registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+
+
+  devise_for :users, path: '',
+             path_names: { sign_out: :logout, sign_in: :login },
+             controllers: {
+                 registrations: 'users/registrations',
+             }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
