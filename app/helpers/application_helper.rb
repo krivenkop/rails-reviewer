a56e780d7f4d5dd2c037d97b444c13cc77076588
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-06 19:29:24 +0200

    ADD: header for authenticated user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def active_link_to(name = nil, path = nil, html_options = nil, &block)
+    active_class = html_options&['active'] || 'active'
+    html_options&.delete(:active)
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(path)
+    link_to(name, path, html_options, &block)
+  end
+
+  def active_link_with_block(path = nil, html_options = nil, &block)
+    active_class = html_options[:active] || 'active'
+    html_options.delete(:active)
+    html_options[:class] = "#{html_options[:class]} #{active_class}" if current_page?(path)
+    link_to(path, html_options, &block)
+  end
 end
