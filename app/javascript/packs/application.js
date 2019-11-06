f4e913fb0e66a93d320ffd99d54a160f676079b8
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-06 16:35:52 +0200

    ADD: devise registration and auth

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 import 'normalize.scss/normalize.scss';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 require('../sass/main.sass');
 
 
