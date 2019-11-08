d2ceb9d5e009f230ec65350978edd140f2e9280a
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-08 15:40:12 +0200

    ADD: login spec

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature "User can login on website", %q{
+  For access to posting and seeing of reviews
+  user need to login on website
+} do
+  given(:user) { User.create(email: 'test@gmail.com', password: '12345678', first_name: 'Alex', last_name: 'Jobs') }
+
+  scenario "Registered user tried to login" do
+    visit new_user_session_path
+
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'login'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario "Unregistered user tried to login" do
+    visit new_user_session_path
+
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: 'not_valid_pass'
+    click_on 'login'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
