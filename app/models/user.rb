ad08110d02d22d774b35be4523e63166442dd490
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-08 16:52:57 +0200

    ADD: username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :username, presence: true
 
   def full_name
     first_name + ' ' + last_name
