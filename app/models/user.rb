599600a1ee490507ef044ffde3cad4f269e30153
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-16 20:52:56 +0200

    FIX: issues from pull request review

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :username, presence: true
 
   def full_name
-    first_name + ' ' + last_name
+    "#{first_name} #{last_name}"
   end
 
   def admin?
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email { "test@gmail.com" }
-    password { "12345678" }
-    first_name { "Alex" }
-    last_name { "Jobs" }
-    username { "alex_jobs" }
-  end
+    email { 'test@gmail.com' }
+    password { '12345678' }
+    first_name { 'Alex' }
+    last_name { 'Jobs' }
+    username { 'alex_jobs' }
 
-  # factory :admin do
-  #   id {2}
-  #   email {"test@admin.com"}
-  #   password {"qwerty"}
-  #   admin {true}
-  # end
+    trait :admin do
+      type { 'Admin' }
+    end
+  end
 end
