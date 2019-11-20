ebfe64e3bc3216cd294121c1a3fd7e05d5459605
krivenkop <paul.krivenko@devexpertsteam.com>
2019-11-20 22:57:56 +0200

    UPDATE: list fetcher service

--- app/services/countries/list_fetcher.rb
+++ app/services/countries/list_fetcher.rb
@@ -2,7 +2,9 @@ require 'json'
 
 class Countries::ListFetcher
   def self.fetch
-    response = Faraday.get 'https://restcountries.eu/rest/v2/all'
-    JSON.parse response.body
+    body = Faraday.get('https://restcountries.eu/rest/v2/all').body
+    countries = JSON.parse(body)
+
+    countries.map(&:symbolize_keys)
   end
 end
