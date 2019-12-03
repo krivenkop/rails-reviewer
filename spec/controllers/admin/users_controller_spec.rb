a8c62f4630e9565024c6b85f82b6b05ef125e6af
lineup <paul.krivenko@devexpertsteam.com>
2019-12-03 21:50:24 +0200

    FIX: controller spec

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
-  let(:user) { create(:user, :admin) }
-  before { sign_in user }
+  let(:admin) { create(:admin) }
+  before { sign_in admin }
 
   describe "GET #index" do
     let(:users) { create_list(:user, 2) }
@@ -10,7 +10,7 @@ RSpec.describe Admin::UsersController, type: :controller do
     before { get :index }
 
     it "gets all array of users" do
-      expect(assigns(:users)).to match_array(users.prepend user)
+      expect(assigns(:users)).to match_array(users.prepend admin)
     end
 
     it "renders template index" do
@@ -22,7 +22,7 @@ RSpec.describe Admin::UsersController, type: :controller do
     before { post :new }
 
     it "assigns a new user to @user" do
-      expect(assigns(:user)).to be_a_new user
+      expect(assigns(:user)).to be_a_new User
     end
 
     it "renders template new" do
@@ -33,9 +33,9 @@ RSpec.describe Admin::UsersController, type: :controller do
   describe "POST #create" do
     context "params is valid" do
       it "saves a new user" do
-        count = user.count
+        count = User.count
         post :create, params: { user: attributes_for(:user) }
-        expect(user.count).to eq count + 1
+        expect(User.count).to eq count + 1
       end
 
       it "redirected to index" do
@@ -46,19 +46,20 @@ RSpec.describe Admin::UsersController, type: :controller do
 
     context 'params is not valid' do
       it "does not save a new user" do
-        count = user.count
-        post :create, params: { user: attributes_for(:user, :invalid_name) }
-        expect(user.count).to eq count
+        count = User.count
+        post :create, params: { user: attributes_for(:user, :invalid_first_name) }
+        expect(User.count).to eq count
       end
 
       it "renders template new" do
-        post :create, params: { user: { name: 'Canada' } }
+        post :create, params: { user: attributes_for(:user, :invalid_first_name)  }
         expect(response).to render_template :new
       end
     end
   end
 
   describe "GET #edit" do
+    let(:user) { create :user }
     it "renders template edit" do
       get :edit, params: { id: user }
       expect(response).to render_template :edit
@@ -66,14 +67,16 @@ RSpec.describe Admin::UsersController, type: :controller do
   end
 
   describe "PATCH #update" do
+    let!(:user) { create(:user) }
+
     it "gets user by id" do
-      patch :update, params: { id: user, user: attributes_for(:user) }
+      patch :update, params: { id: user, user: attributes_for(:user, :another_first_name) }
       expect(assigns(:user)).to eq(user)
     end
 
     context "params is valid" do
       it "successfully updates model" do
-        attributes = attributes_for :user
+        attributes = attributes_for :user, :another_first_name
         patch :update, params: { id: user, user: attributes }
         user.reload
 
@@ -87,7 +90,7 @@ RSpec.describe Admin::UsersController, type: :controller do
     end
 
     context "params is not valid" do
-      before { patch :update, params: { id: user, user: attributes_for(:user, :another_first_name) } }
+      before { patch :update, params: { id: user, user: attributes_for(:user, :invalid_first_name) } }
 
       it "does not update model" do
         user.reload
@@ -102,19 +105,19 @@ RSpec.describe Admin::UsersController, type: :controller do
   end
 
   describe "DELETE #destroy" do
-    let!(:person) { create(:user) }
+    let!(:user) { create(:user) }
 
     it "gets user by id" do
-      delete :destroy, params: { id: person }
-      expect(assigns(:person)).to eq(person)
+      delete :destroy, params: { id: user }
+      expect(assigns(:user)).to eq(user)
     end
 
     it "successfully deletes user" do
-      expect { delete :destroy, params: { id: person } }.to change(person, :count).by(-1)
+      expect { delete :destroy, params: { id: user } }.to change(User, :count).by(-1)
     end
 
     it "redirects to index" do
-      delete :destroy, params: { id: person }
+      delete :destroy, params: { id: user }
       expect(response).to redirect_to admin_users_path
     end
   end
